c46affb857f631393511bca9a0ed11231f7b50ee
muellerdo <domey038@live.de>
2022-01-06 13:15:56 +0100

    Created draft for __init__

---
+++ miseval/__init__.py
@@ -0,0 +1,37 @@
+#==============================================================================#
+#  Author:       Dominik Müller                                                #
+#  Copyright:    2022 IT-Infrastructure for Translational Medical Research,    #
+#                University of Augsburg                                        #
+#                                                                              #
+#  This program is free software: you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by        #
+#  the Free Software Foundation, either version 3 of the License, or           #
+#  (at your option) any later version.                                         #
+#                                                                              #
+#  This program is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+#  GNU General Public License for more details.                                #
+#                                                                              #
+#  You should have received a copy of the GNU General Public License           #
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#==============================================================================#
+#-----------------------------------------------------#
+#                   Metric Imports                    #
+#-----------------------------------------------------#
+# Confusion Matrix
+from miseval.confusion_matrix import calc_ConfusionMatrix, \
+                                     calc_FalseNegative, calc_FalsePositive, \
+                                     calc_TrueNegative, calc_TruePositive
+
+#-----------------------------------------------------#
+#         Access Functions to Metric Functions        #
+#-----------------------------------------------------#
+# Metric Dictionary
+metric_dict = {
+    "ConfusionMatrix": calc_ConfusionMatrix,
+    "TruePositive": calc_TruePositive,
+    "TrueNegative": calc_TrueNegative,
+    "FalsePositive": calc_FalsePositive,
+    "FalseNegative": calc_FalseNegative
+}
