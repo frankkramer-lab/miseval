f25a93df532f553894cc476b4d3f74c8ad7b021c
muellerdo <domey038@live.de>
2022-01-10 23:38:55 +0100

    Added some docu on evaluate functionality

--- miseval/core.py
+++ miseval/core.py
@@ -32,21 +32,38 @@ from miseval import metric_dict
 Metric Parameter:
     The miseval eavluate function can be run with different metrics as backbone.
     You can pass the following options to the metric parameter:
-    - String naming one of the metric labels, for example "ConfusionMatrix"
-    - Directly passing a metric function, for example calc_ConfusionMatrix (from confusion_matrix.py)
+    - String naming one of the metric labels, for example "DSC"
+    - Directly passing a metric function, for example calc_DSC (from dice.py)
     - Passing a custom metric function
 
     List of metrics : See miseval/__init__.py under section "Access Functions to Metric Functions"
 
+    The classes in a segmentation mask must be ongoing starting from 0 (integers from 0 to n_classes-1).
+
+    A segmentation mask is allowed to have either no channel axis or just 1 (e.g. 512x512x1),
+    which contains the annotation.
+    The only exception from this is by activating the probabilities parameter, which results
+    that a segmentation mask must have the same number of channels as n_classes (e.g. 512x512x8 if n_classes==8).
+    Also the probabilities have to be in range between 0 to 1 and sum up to 1 (softmax).
+
 Arguments:
-    truth (NumPy Matrix):           A data generator which will be used for inference.
-    pred (NumPy Matrix):             Instance of a AUCMEDI neural network class.
-    metric (String or Function):                NumPy Array of classification prediction encoded as OHE (output of a AUCMEDI prediction).
-    multi_class (Boolean):                    XAI method class instance or index. By default, GradCAM is used as XAI method.
-    classes (Integer):                 Layer name of the convolutional layer for heatmap computation. If None, the last conv layer is used.
-    probabilities (Boolean):                      Transparency value for heatmap overlap plotting on input image (range: [0-1]).
+    truth (NumPy Matrix):            Ground Truth segmentation mask.
+    pred (NumPy Matrix):             Prediction segmentation mask.
+    metric (String or Function):     Metric function. Either a function directly or encoded as String from miseval or a custom function.
+    multi_class (Boolean):           Boolean parameter, if segmentation is a binary or multi-class problem. By default False -> Binary mode.
+    n_classes (Integer):             Number of classes. By default 2 -> Binary
+    probabilities (Boolean):         Boolean parameter, if predicted segmentation (pred) is encoded as softmax output.
+                                     By default False -> normal class vector expected.
+
+Output:
+    score (Float) or scores (List of Float)
+
+    The multi_class parameter defines the output of this function.
+    If n_classes > 2, multi_class is automatically True.
+    If multi_class == False & n_classes == 2, only a single score (float) is returned.
+    If multi_class == True, multiple scores as a list are returned (for each class one score).
 """
-def evaluate(truth, pred, metric, multi_class=False, classes=2,
+def evaluate(truth, pred, metric, multi_class=False, n_classes=2,
              probabilities=False):
     # Initialize metric function
     if isinstance(metric, str) and metric in metric_dict:
