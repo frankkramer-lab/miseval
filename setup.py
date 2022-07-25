8325b2616709220eac1ee51fa6720cbc574f41d4
muellerdo <domey038@live.de>
2022-07-25 19:52:08 +0200

    build: Updated to version v1.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
    name='miseval',
-   version='1.2.1',
+   version='1.2.2',
    description='A Metric Library for Medical Image Segmentation Evaluation',
    url='https://github.com/frankkramer-lab/miseval',
    author='Dominik Müller',
